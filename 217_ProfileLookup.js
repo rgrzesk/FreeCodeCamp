dc0fd3fb20e710dfce46a83ecb0cba83947f86de
rgrzeskowiak <robert.grzeskowiak@cognifide.com>
2016-11-17 13:31:34 +0100

    217 added

---
+++ 217_ProfileLookup.js
@@ -0,0 +1,55 @@
+
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop){
+// Only change code below this line
+var toDisplay;
+for(var i=0; i<contacts.length; i++) {
+
+if(contacts[i].firstName == firstName) {
+    if(contacts[i][prop]!==undefined) {
+        toDisplay = contacts[i][prop];
+        break;
+    }
+    toDisplay = "No such property";
+    break;
+}
+toDisplay = "No such contact";
+}
+return toDisplay;
+// Only change code above this line
+}
+
+// Change these values to test your function
+console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Sherlock", "likes"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Bob", "number"));
+console.log(lookUpProfile("Akira", "address"));
