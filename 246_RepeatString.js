18cec397a49e2f698a50a0dab2f7ddc98f2f214c
rgrzeskowiak <robert.grzeskowiak@cognifide.com>
2016-11-21 19:58:10 +0100

    repeat string

---
+++ 246_RepeatString.js
@@ -0,0 +1,20 @@
+/*
+Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Global String Object
+*/
+
+
+function repeatStringNumTimes(str, num) {
+  var newStr ="";
+  for(var i=0; i<num; i++) {
+      newStr += str;
+  }
+  return newStr;
+}
+
+repeatStringNumTimes("abc", 3);
